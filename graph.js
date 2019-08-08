fc42469678e4dd758f4e5ab96e62be9805e9cac0
Yeongmin <oinochoe@naver.com>
2019-08-08 16:36:41 +0900

    graph 개선 data.json 삭제 및 전체 개선

--- graph.js
+++ graph.js
@@ -1,14 +1,10 @@
-var svg = d3.select("svg"),
-   // basic margin
-    margin = {
-      top: 20,
-      right: 120,
-      bottom: 30,
-      left: 140
-    },
-    // default width, height setting
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom;
+// default
+var margin = { top: 20, right: 20, bottom: 30, left: 40}
+var width = 1000 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+var svg = d3.select(".graph").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom).call(responsiveSvg)
 
 // time Parsing style
 var parseTime = d3.timeParse("%Y-%m-%d");
@@ -18,11 +14,11 @@ bisectDate = d3.bisector(function (d) {
   return d.days;
 }).left;
 
-// range setting..
+// range setting
 var x = d3.scaleTime().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
 
-// line value setting..
+// line value setting.
 var line = d3.line().x(function (d) {
   return x(d.days);
 }).y(function (d) {
@@ -30,36 +26,64 @@ var line = d3.line().x(function (d) {
 });
 
 // g = svg's group and positioning
-var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var g = svg.append("g").classed("group", true).attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// get data from json (you can change the extracting method)
-d3.json("data.json", function (error, data) {
-  if (error) throw error;
+// get Data
+var data = [{
+  "days": "2019-07-02",
+  "value": 72
+}, {
+  "days": "2019-07-03",
+  "value": 720
+}, {
+  "days": "2019-07-04",
+  "value": 720
+}]
 
-  // data parsing time and value setting
-  data.forEach(function (d) {
-    d.days = parseTime(d.days);
-    d.value = +d.value;
-  });
+// data binding
+data.forEach(function (d) {
+  d.days = parseTime(d.days);
+  d.value = +d.value;
+});
 
-  // days
-  x.domain(d3.extent(data, function (d) {
-    return d.days;
-  }));
+// days
+x.domain(d3.extent(data, function (d) {
+  return d.days;
+}));
 
-  // value
-  y.domain([d3.min(data, function (d) {
-    return d.value;
-  }) / 1.005, d3.max(data, function (d) {
-    return d.value;
-  }) * 1.005]);
+// value
+y.domain([d3.min(data, function (d) {
+  return d.value;
+}) / 1.005, d3.max(data, function (d) {
+  return d.value;
+}) * 1.005]);
 
-  // Add designing and drawing
-  g.append("g").attr("class", "axis axis--x").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x).tickFormat(d3.timeFormat("%m/%d")));
-  g.append("g").attr("class", "axis axis--y").call(make_y_gridlines().tickSize(-width));
-  g.append("path").datum(data).attr("class", "line").attr("d", line);
-  // GRADIENT START
-  g.append("linearGradient")
+// Add designing and drawing
+g.append("g").attr("class", "axis axis--x").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x).tickFormat(d3.timeFormat("%m/%d")));
+g.append("g").attr("class", "axis axis--y").call(d3.axisLeft(y).tickFormat(d3.format(",.1s")));
+
+// d3 label per unit
+var lasttext;
+d3.select('.axis--y').select('text').each(function(){
+  lasttext = this.textContent.split('').pop();
+});
+
+if (lasttext !== "" && lasttext !== /[0-9]/g) {
+  if (lasttext === "k") {
+    document.querySelector('.unit').innerText = "(단위 : 1,000)";
+  } else if (lasttext === "M") {
+    document.querySelector('.unit').innerText = "(단위 : 1,000,000)";
+  } else if (lasttext === "G") {
+    document.querySelector('.unit').innerText = "(단위 : 1,000,000,000)";
+  } else if (lasttext === "T") {
+    document.querySelector('.unit').innerText = "(단위 : 1,000,000,000,000)";
+  }
+}
+
+//call(make_y_gridlines().tickSize(-width));
+g.append("path").datum(data).attr("class", "line").attr("d", line);
+// GRADIENT START
+g.append("linearGradient")
   .attr("id", "graph-gradient")
   .attr("gradientUnits", "userSpaceOnUse")
   .attr("x1", 0)
@@ -76,45 +100,42 @@ d3.json("data.json", function (error, data) {
   .append("stop")                             // stop Positioning
   .attr("offset", d => d.offset)              // offset setting
   .attr("stop-color", d => d.color);          // stop color
-  // GRADIENT END
+// GRADIENT END
 
-  /*
-   * Function List
-   */
+/*
+ * Function List
+ */
 
-   // gridLines each 5 lens
-  function make_y_gridlines() {
-    return d3.axisLeft(y)
-        .ticks(5)
-  }
+// gridLines each 5 lens
+function make_y_gridlines() {return d3.axisLeft(y).ticks(5)}
 
-  // getMax value from arrays
-  function getMax(arr, prop) {
-      var max;
-      for (var i=0 ; i<arr.length ; i++) {
-          if (!max || parseInt(arr[i][prop]) > parseInt(max[prop]))
-              max = arr[i];
-      }
-      return max;
-  }
+// getMax value from arrays
+function getMax(arr, prop) {
+    var max;
+    for (var i=0 ; i<arr.length ; i++) {
+        if (!max || parseInt(arr[i][prop]) > parseInt(max[prop]))
+            max = arr[i];
+    }
+    return max;
+}
 
-  // getMin value from arrays
-  function getMin(arr, prop) {
-      var min;
-      for (var i=0 ; i<arr.length ; i++) {
-          if (!min || parseInt(arr[i][prop]) < parseInt(min[prop]))
-              min = arr[i];
-      }
-      return min;
-  }
+// getMin value from arrays
+function getMin(arr, prop) {
+    var min;
+    for (var i=0 ; i<arr.length ; i++) {
+        if (!min || parseInt(arr[i][prop]) < parseInt(min[prop]))
+            min = arr[i];
+    }
+    return min;
+}
 
-  // focusing position (Mouse over, move, touch over and move)
-  var focus = g.append("g").attr("class", "focus").style("display", "none");
+// focusing position (Mouse over, move, touch over and move)
+var focus = g.append("g").attr("class", "focus").style("display", "none");
 
-  focus.append("circle").attr("r", 1.5);
-  focus.append("text").attr("x", 15).attr("dy", ".3em");
-  
-  svg.append("rect").attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+focus.append("circle").attr("r", 1.5);
+focus.append("text").attr("x", 15).attr("dy", ".3em");
+
+svg.append("rect").attr("transform", "translate(" + margin.left + "," + margin.top + ")")
   .style('cursor', 'pointer')
   .attr("class", "overlay").attr("width", width).attr("height", height)
   .on("mouseover touchstart", function () {
@@ -123,15 +144,48 @@ d3.json("data.json", function (error, data) {
     focus.style("display", "none");
   }).on("mousemove touchmove", mousemove);
 
-  function mousemove() {
-    var x0 = x.invert(d3.mouse(this)[0]),
-      i = bisectDate(data, x0, 1),
-      d0 = data[i - 1],
-      d1 = data[i],
-      d = x0 - d0.days > d1.days - x0 ? d1 : d0;
+function mousemove() {
+  var x0 = x.invert(d3.mouse(this)[0]),
+    i = bisectDate(data, x0, 1),
+    d0 = data[i - 1],
+    d1 = data[i],
+    d = x0 - d0.days > d1.days - x0 ? d1 : d0;
     focus.attr("transform", "translate(" + x(d.days) + "," + y(d.value) + ")");
     focus.select("text").text(function () {
-      return d.value;
-    });
+    return d.value.toString().replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,');
+  });
+}
+
+// resize function
+function responsiveSvg(svg, maxw) {
+  // get container + svg aspect
+  var container = d3.select(svg.node().parentNode),
+      width = parseInt(svg.style("width")),
+      height = parseInt(svg.style("height")),
+      aspect = width / height;
+
+  // add viewBox and preserveAspectRatio properties
+  // and call resize so that svg resizes on initial page load
+  svg.attr("viewBox", "0 0 " + width + " " + height)
+    .attr("preserveAspectRatio", "xMinYMid")
+    .call(resize);
+
+  // to register multiple listeners
+  d3.select(window).on("resize." + container.attr("id"), resize);
+
+  // get width of container and resize svg to fit it
+  function resize() {
+    var targetWidth = parseInt(container.style("width"));
+    svg.attr("width", targetWidth);
+    svg.attr("height", Math.round(targetWidth / aspect));
   }
-});
+
+  // trigger resize event
+  var el = document;
+  var event = document.createEvent("HTMLEvents");
+  event.initEvent("resize", true, false);
+  el.dispatchEvent(event);
+}
+// when modal open window resize dispatch
+// window.dispatchEvent(new Event("resize"));
+
