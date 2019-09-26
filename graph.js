09c66d8916fc016d2615c4cb6e63fff1a89420b2
Yeongmin <copstyle86@gmail.com>
2019-09-26 15:42:30 +0900

    iphone7 test

--- graph.js
+++ graph.js
@@ -94,9 +94,9 @@ var drawgraph = (function(priceData) {
       .attr("y2", y(getMax(priceData, "value").value)) // maxValue
       .selectAll("stop")                          // setting stop
       .data([
-        { offset: "0%", color: "blue" },
-        { offset: "51%", color: "white" },
-        { offset: "100%", color: "red" }
+        { offset: "0%", color: "#0000ff" },
+        { offset: "50%", color: "#ffffff" },
+        { offset: "100%", color: "#ff0000" }
       ])
       .enter()
       .append("stop")                             // stop Positioning
